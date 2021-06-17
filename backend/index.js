187672b14c796afa4aa9bc5e45665823c0b1fdcc
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-06-17 08:40:09 +0300

    Alustava backend runko

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const http = require('http')
+
+let demodata = [
+  {
+    id: 1,
+    story: "As a user I want to create new user stories",
+    priority: 1,
+    date: "2021-06-17T08:30:31.123Z",
+    status: "done"
+  },
+  {
+    id: 2,
+    story: "As a user I want to modify user stories",
+    priority: 2,
+    date: "2021-06-17T08:32:22.221Z",
+    status: "in progress"
+  },
+  {
+    id: 3,
+    story: "As a user I want to delete user stories",
+    priority: 99,
+    date: "2021-06-17T08:34:12.091Z",
+    status: "not started"
+  },
+]
+
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(demodata))
+})
+
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
