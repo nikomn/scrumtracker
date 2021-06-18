7a04da2c6006a337ee19aa861debc38b8edce283
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-06-18 16:27:26 +0300

    Sovellus Herokuun ensimmäinen yritys

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ const app = http.createServer((request, response) => {
 })
 
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
