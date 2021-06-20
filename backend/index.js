6c4454298f693b2ae444ea869df3862f11961ffb
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-06-20 13:23:55 +0300

    Alustava Eslint setup

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,31 @@ app.use(express.static('build'))
 let demodata = [
   {
     id: 1,
-    story: "As a user I want to create new user stories",
+    story: 'As a user I want to create new user stories',
     priority: 1,
-    date: "2021-06-17T08:30:31.123Z",
-    status: "done"
+    date: '2021-06-17T08:30:31.123Z',
+    status: 'done'
   },
   {
     id: 2,
-    story: "As a user I want to modify user stories",
+    story: 'As a user I want to modify user stories',
     priority: 2,
-    date: "2021-06-17T08:32:22.221Z",
-    status: "in progress"
+    date: '2021-06-17T08:32:22.221Z',
+    status: 'in progress'
   },
   {
     id: 3,
-    story: "As a user I want to delete user stories",
+    story: 'As a user I want to delete user stories',
     priority: 99,
-    date: "2021-06-17T08:34:12.091Z",
-    status: "not started"
+    date: '2021-06-17T08:34:12.091Z',
+    status: 'not started'
   },
   {
     id: 4,
-    story: "Testing automatic deployment to heroku...",
+    story: 'Testing automatic deployment to heroku...',
     priority: 1,
-    date: "2021-06-17T08:30:31.123Z",
-    status: "done"
+    date: '2021-06-17T08:30:31.123Z',
+    status: 'done'
   }
 ]
 
@@ -43,6 +43,7 @@ app.get('/api/userstorys', (req, res) => {
 })
 
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
