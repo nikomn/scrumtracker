35cf7b33f68fa242bbf35b25dd068093c343455b
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-07-03 10:34:41 +0300

    Tietokantaan tallentaminen

--- backend/models/userstory.js
+++ backend/models/userstory.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+//console.log('connecting to', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
     console.log('connected to MongoDB')
