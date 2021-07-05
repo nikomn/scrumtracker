f63bfe623cc63793d4ae74527841e4042ac954b8
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-07-05 16:32:00 +0300

    Refaktorointi IV, app.js

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const userStoriesRouter = require('./controllers/userstories')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/userstories', userStoriesRouter)
+
+module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,10 @@
-require('dotenv').config()
-const UserStory = require('./models/userstory')
-const express = require('express')
-const app = express()
-app.use(express.static('build'))
-app.use(express.json())
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const cors = require('cors')
+const server = http.createServer(app)
 
-app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('<h1>Scrum Tracker app backend</h1>')
-})
-
-app.get('/api/userstories', (req, res) => {
-  UserStory.find({}).then(stories => {
-    res.json(stories)
-  })
-})
-
-
-app.post('/api/userstories', (request, response) => {
-  //console.log(request.body)
-  const body = request.body
-
-  if (!body.story) {
-    return response.status(400).json({ 
-      error: 'story is empty' 
-    })
-  }
-
-  const newStoryObject = new UserStory({
-    story: body.story,
-    date: new Date(),
-    status: 'new',
-    priority: 9999
-  })
-
-  newStoryObject.save().then(savedUserStory => {
-    response.json(savedUserStory)
-  })
-
-
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
