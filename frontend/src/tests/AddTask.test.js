708ac602fa473ff25d4dbad67a77fc65d920f36b
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-08-10 09:02:55 +0300

    Frontend AddTask testit

---
+++ frontend/src/tests/AddTask.test.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import AddTask from '../components/AddTask'
+
+test('<AddTask /> renders', () => {
+
+  const story = {
+    id: 1,
+    story: 'Testing with Jest',
+    priority: 1,
+    date: '2021-06-17T08:30:31.123Z',
+    status: 'done',
+    tasks: []
+  }
+
+  const component = render(
+    <AddTask
+      createNewTask={null}
+      story={story.id} />
+  )
+
+  expect(component.container).toHaveTextContent(
+    'Add new Task'
+  )
+})
+
+
+test('<AddTask /> updates parent state and calls onSubmit', () => {
+  const mockHandler = jest.fn()
+
+  const story = {
+    id: 1,
+    story: 'Testing with Jest',
+    priority: 1,
+    date: '2021-06-17T08:30:31.123Z',
+    status: 'done',
+    tasks: []
+  }
+
+  const component = render(
+    <AddTask
+      createNewTask={mockHandler}
+      story={story.id} />
+  )
+
+  const taskinput = component.container.querySelector('#task-input')
+  const taskstatusinput = component.container.querySelector('#task-status-input')
+  const form = component.container.querySelector('form')
+
+  fireEvent.change(taskinput, {
+    target: { value: 'testing adding of new task' }
+  })
+  fireEvent.change(taskstatusinput, {
+    target: { value: 'testing' }
+  })
+
+  fireEvent.submit(form)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+  expect(mockHandler.mock.calls[0][0].name).toBe('testing adding of new task')
+  expect(mockHandler.mock.calls[0][0].status).toBe('testing')
+})
+
