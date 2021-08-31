ceea4258595bbeed6a9c1b57f02160775e348a60
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2021-08-31 09:17:56 +0300

    Testejä AddMaintenanceStory komponentille

--- frontend/src/tests/AddMaintenanceStory.test.js
+++ frontend/src/tests/AddMaintenanceStory.test.js
@@ -8,10 +8,10 @@ test('<AddMaintenanceStory /> renders', () => {
     name: 'test'
   }
 
-  // createNewStory, addStoryToSprintBacklog, backlog
+  // backlog, createMaintenanceStory
 
   const component = render(
-    <AddMaintenanceStory createNewStory={null} addStoryToSprintBacklog={null} backlog={backlog} />
+    <AddMaintenanceStory createMaintenanceStory={null} backlog={backlog} />
   )
 
   expect(component.container).toHaveTextContent(
@@ -19,65 +19,63 @@ test('<AddMaintenanceStory /> renders', () => {
   )
 })
 
-test('clicking add story button calls event handlers once', async () => {
+test('clicking add story button calls event handler once', async () => {
   const backlog = {
-    name: 'test'
+    name: 'test',
+    id: 1
   }
 
-  const mockCreateHandler = jest.fn()
-  const mockAddHandler = jest.fn()
+  const mockHandler = jest.fn()
 
   const component = render(
-    <AddMaintenanceStory createNewStory={mockCreateHandler} addStoryToSprintBacklog={mockAddHandler} backlog={backlog} />
+    <AddMaintenanceStory createMaintenanceStory={mockHandler} backlog={backlog} />
   )
 
   const button = component.getByText('Add new story')
   fireEvent.click(button)
 
-  expect(mockCreateHandler.mock.calls).toHaveLength(1)
-  //expect(mockAddHandler.mock.calls).toHaveLength(1)
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
 
 
-// test('<AddMaintenanceStory /> updates parent state and calls onSubmit', () => {
-//   const backlog = {
-//     name: 'test',
-//     id: 1
-//   }
+test('<AddMaintenanceStory /> updates parent state and calls onSubmit', () => {
+  const backlog = {
+    name: 'test',
+    id: 1
+  }
 
-//   const mockCreateHandler = jest.fn()
-//   const mockAddHandler = jest.fn()
+  const mockHandler = jest.fn()
 
-//   const component = render(
-//     <AddMaintenanceStory createNewStory={mockCreateHandler} addStoryToSprintBacklog={mockAddHandler} backlog={backlog} />
-//   )
+  const component = render(
+    <AddMaintenanceStory createMaintenanceStory={mockHandler} backlog={backlog} />
+  )
 
-//   const storyinput = component.container.querySelector('#story-input')
-//   const priorityinput = component.container.querySelector('#priority-input')
-//   //const statusinput = component.container.querySelector('#status-input')
-//   const statusdropdown = component.container.querySelector('#status-dropdown')
-//   const form = component.container.querySelector('form')
+  const storyinput = component.container.querySelector('#story-input')
+  const priorityinput = component.container.querySelector('#priority-input')
+  //const statusinput = component.container.querySelector('#status-input')
+  const statusdropdown = component.container.querySelector('#status-dropdown')
+  const form = component.container.querySelector('form')
 
-//   fireEvent.change(storyinput, {
-//     target: { value: 'testing adding of new maintenance story' }
-//   })
-//   fireEvent.change(priorityinput, {
-//     target: { value: 123 }
-//   })
-//   fireEvent.change(statusdropdown, {
-//     target: { value: 'done' }
-//   })
+  fireEvent.change(storyinput, {
+    target: { value: 'testing adding of new maintenance story' }
+  })
+  fireEvent.change(priorityinput, {
+    target: { value: 123 }
+  })
+  fireEvent.change(statusdropdown, {
+    target: { value: 'done' }
+  })
 
 
-//   fireEvent.submit(form)
+  fireEvent.submit(form)
 
-//   console.log(mockAddHandler.mock.calls[0])
+  expect(mockHandler.mock.calls).toHaveLength(1)
+  //console.log(mockHandler.mock.calls[0][0])
+  //console.log(mockHandler.mock.calls[0])
+  expect(mockHandler.mock.calls[0][0].story).toBe('testing adding of new maintenance story' )
+  expect(mockHandler.mock.calls[0][0].priority).toBe('123')
+  expect(mockHandler.mock.calls[0][0].status).toBe('done')
+  expect(mockHandler.mock.calls[0][1].name).toBe('test')
+  expect(mockHandler.mock.calls[0][1].id).toBe(1)
 
-//   expect(mockCreateHandler.mock.calls).toHaveLength(1)
-//   //console.log(mockHandler.mock.calls[0][0])
-//   //console.log(mockHandler.mock.calls[0])
-//   expect(mockCreateHandler.mock.calls[0][0].story).toBe('testing adding of new user story' )
-//   expect(mockCreateHandler.mock.calls[0][0].priority).toBe('123')
-//   expect(mockCreateHandler.mock.calls[0][0].status).toBe('done')
-
-// })
+})
